f0330b1789493c38a81bb863ddc45bf07f284c29
Gooey <caethan@gmail.com>
2021-11-28 20:20:31 -0600

    MovementV1

---
+++ Assets/Assets/Scripts/MushyBarrier.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushyBarrier : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    GameObject Player;
+    public GameObject Mushy;
+    public bool Gold = false;
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Mushy.GetComponent<MushyScript>().speed;
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Gold = Mushy.GetComponent<MushyScript>().Gold;
+
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Mushy")
+        {
+            Mushy.transform.position = Vector2.MoveTowards(Mushy.transform.position, other.gameObject.transform.position, -1 * (speed * 2) * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Assets/Scripts/MushyScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushyScript : MonoBehaviour
+{
+    GameObject Player;
+    public GameObject GoldiLocks;
+    public float speed = 10f;
+    public Rigidbody2D rb;
+    public bool Gold = false;
+    public bool InBar = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Gold = GoldiLocks.GetComponent<MushyGoldilocks>().Gold;
+
+        if (Gold == false)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Player.transform.position, speed * Time.deltaTime);
+        }
+        
+    }
+
+}
